2fb353805528257e1bcaf576bde6d4b84c588d44
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2025-10-27 23:31:10 +0900

    PPMI 作成

--- src/common/util.py
+++ src/common/util.py
@@ -1,55 +1,108 @@
 import numpy as np
 
-class Utility:
 
-    def preprocess(text):
-        text = text.lower()
-        text.replace('.', ' .')
-        words = text.split(' ')
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
 
-        word_to_id = {}
-        id_to_word = {}
+    word_to_id = {}
+    id_to_word = {}
 
-        for word in words:
-            if word not in word_to_id:
-                new_id = len(word_to_id)
-                word_to_id[word] = new_id
-                id_to_word[new_id] = word
-        
-        corpus = np.array([word_to_id[w] for w in words])
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+    
+    corpus = np.array([word_to_id[w] for w in words])
 
-        return corpus, word_to_id, id_to_word
+    return corpus, word_to_id, id_to_word
 
-    # cosine 類似度の算出
-    # :param x: ベクトル
-    # :param y: ベクトル
-    # :param eps: 0 割り防止のための微小値
-    # return:
-    def cos_similarity(x, y, eps=1e-8):
-        nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
-        ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
-        return np.dot(nx, ny)
 
-    # 共起行列の作成
-    # param: corpus: コーパス(単語 ID リスト)
-    # param: vocab_size: 語彙数
-    # param: window_size: ウィンドウサイズ(ウィンドウサイズが 1 のとき単語の左右 1 単語がコンテキスト)
-    # return: 共起行列
-    def create_co_matrix(corpus, vocab_size, window_size=1):
-        corpus_size = len(corpus)
-        co_matrix = np.zeros(vocab_size, vocab_size, dtype=np.int32)
+# cosine 類似度の算出
+# :param x: ベクトル
+# :param y: ベクトル
+# :param eps: 0 割り防止のための微小値
+# return:
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
 
-        for idx, word_id in enumerate(corpus):
-            for i in range(1, window_size):
-                left_idx = idx - i
-                right_idx = idx + i
 
-                if left_idx >= 0:
-                    left_word_id = corpus[left_idx]
-                    co_matrix[idx, left_word_id] += 1
-                
-                if right_idx < corpus_size:
-                    right_word_id = corpus[right_idx]
-                    co_matrix[idx, right_word_id] += 1
+# 類似単語の検索
+# param: query: クエリ(テキスト)
+# param: word_to_id: 単語から単語 ID へのディクショナリ
+# param: id_to_word: 単語 ID から単語へのディクショナリ
+# param: word_matrix: 単語ベクトルをまとめた行列。各行に対応する単語のベクトルが格納されていることを想定
+# param: top: 上位何位魔で表示するか
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    if query not in word_to_id:
+        print('%s is not found' % query)
+        return
+    
+    print('\n[query] ' + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+    
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(' %s: %s' % (id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return
+
+# 共起行列の作成
+# param: corpus: コーパス(単語 ID リスト)
+# param: vocab_size: 語彙数
+# param: window_size: ウィンドウサイズ(ウィンドウサイズが 1 のとき単語の左右 1 単語がコンテキスト)
+# return: 共起行列
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
             
-        return co_matrix
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+        
+    return co_matrix
+
+# PPMI (正の相互情報量)の作成
+# param C: 共起行列
+# param verbose: 進行状況を出力するかどうか
+# return: PPMI 行列
+def ppmi(C, verbose=False, eps= 1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100 + 1) == 0:
+                    print('%.1f%% done' % (100*cnt/total))
+    return M
--- src/main.py
+++ src/main.py
@@ -1,21 +1,36 @@
+import numpy as np
 from dataset import spiral
 from common.trainer import Trainer
+from common.util import preprocess, create_co_matrix, cos_similarity, ppmi
 from optimizers.sgd import SGD
 from two_layer_net import TwoLayerNet
 
 def main():
-    max_epoch = 300
-    batch_size = 30
-    hidden_size = 10
-    learning_rate = 1.0
+    # max_epoch = 300
+    # batch_size = 30
+    # hidden_size = 10
+    # learning_rate = 1.0
 
-    x, t = spiral.load_data()
-    model = TwoLayerNet(input_size=2, hidden_size=hidden_size, output_size=3)
-    optimizer = SGD(lr=learning_rate)
+    # x, t = spiral.load_data()
+    # model = TwoLayerNet(input_size=2, hidden_size=hidden_size, output_size=3)
+    # optimizer = SGD(lr=learning_rate)
 
-    trainer = Trainer(model, optimizer)
-    trainer.fit(x, t, max_epoch, batch_size, eval_interval=10)
-    trainer.plot()
+    # trainer = Trainer(model, optimizer)
+    # trainer.fit(x, t, max_epoch, batch_size, eval_interval=10)
+    # trainer.plot()
+    text = 'You say goodbye and I say hello.'
+    corpus, word_to_id, id_to_word = preprocess(text)
+    vocab_size = len(word_to_id)
+    C = create_co_matrix(corpus, vocab_size)
+    W = ppmi(C)
+
+    np.set_printoptions(precision=3)
+    print('covarience matrix')
+    print(C)
+    print('-'*50)
+    print('PPMI')
+    print(W)
+
 
 
 if __name__ == '__main__':
