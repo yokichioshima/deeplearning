c043997da5a65203db4313ca8288633265ffea2f
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2025-10-27 23:43:33 +0900

    SVD で次元削減

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from dataset import spiral
 from common.trainer import Trainer
@@ -23,6 +24,14 @@ def main():
     vocab_size = len(word_to_id)
     C = create_co_matrix(corpus, vocab_size)
     W = ppmi(C)
+
+    U, S, V = np.linalg.svd(W)
+
+    for word, word_id in word_to_id.items():
+        plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+    
+    plt.scatter(U[:, 0], U[:, 1], alpha=0.5)
+    plt.savefig('figure.png')
 
     np.set_printoptions(precision=3)
     print('covarience matrix')
