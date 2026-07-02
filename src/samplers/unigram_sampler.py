e0f166d85b4af5ee9cbbe2780560cca8f522c35a
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2026-07-02 21:24:34 +0900

    add: layer 追加

---
+++ src/samplers/unigram_sampler.py
@@ -0,0 +1,40 @@
+import collections
+
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+        
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+        
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+        self.word_p = self.word_p ** power
+        self.word_p /= np.sum(self.word_p)
+    
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+
+        if not GPU:
+            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+            for i in range(batch_size):
+                p = self.word_p.copy()
+                target_idx = target[i]
+                p[target_idx] = 0
+                p /= np.sum(p)
+                negative_sample[i, :] = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size), 
+                    replace=False, p=self.word_p)
+        else:
+            # GPU(cupy) で計算するときは、速度を優先
+            # 負例にターゲットが含まれることがある
+            negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size),
+                replace=True, p=self.word_p)
+        
+        return negative_sample
